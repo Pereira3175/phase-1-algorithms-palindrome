2919293b875719eebbbe3484c5b0a92a5e9473e5
Luis <pereira1214@gmail.com>
2021-10-13 00:36:57 -0400

    commit tree 538084837a70610a509f9dee5a4b677b63b4adc1

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+let word;
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+    // Write your algorithm here
+    let backWords = reverseWord(word)
+    if (word == backWords)  {
+      return true
+      console.log ("input: " + word )
+      console.log ("output: " + true)
+    } else {
+      return false
+      console.log ("input: " + word)
+      console.log ("output: " + false)
+    }
+    return word
+  }
+  // function to reverse string
+  function reverseWord(str) {
+    return str.split("").reverse().join("");
+  }
 
 /* 
   Add your pseudocode here
+write function name isPalindrome
+write a function to reverse word
+if word arugment is a Palindrome
+then let Palindrome equal true
+else return false
 */
 
 /*
   Add written explanation of your solution here
+
+  Take the word arugment, then set a condition.
+  If the word is equal to the word written backword
+  Then return true
 */
 
 // You can run `node index.js` to view these console logs
